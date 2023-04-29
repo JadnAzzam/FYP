bbe3349f9ab96d7d5e8f691aa0aefced17f88ff9
JadnAzzam <jna26@mail.aub.edu>
2023-04-29 20:54:37 +0300

    fixed the navigation bbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useTranslation } from 'react-i18next';
+import './Navigation.css';
 
 
 function Navigation() {
@@ -55,49 +56,29 @@ function Navigation() {
   
 
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#action">ImaginationStation</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/Home">Home</Nav.Link>
-            <NavDropdown title="dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Categories</NavDropdown.Item>
-              <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
-              {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/signin">
-                LogOut
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-
-        {/* <div>
-        <button onClick={()=>handleClick('en')}>English</button>
-        <button onClick={()=>handleClick('ar')}>Arabic</button>
-        <button onClick={()=>handleClick('fr')}>French</button>
-        </div> */}
-
-        <div>
-          <label>
-            <input type="checkbox" value="en" onChange={handleCheckboxChange}/>English
-          </label>
-          <label>
-            <input type="checkbox" value="ar" onChange={handleCheckboxChange}/>Arabic
-          </label>
-          <label>
-            <input type="checkbox" value="fr" onChange={handleCheckboxChange}/>French
-          </label>
-        </div>
-
-
-        <p>{t('Hello')}</p>
-        
-      </Container>
+    <div>
       
-    </Navbar>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+          <a class="navbar-brand mar" href="#action">ImaginationStation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="/Home">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <NavDropdown title="dropdown" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">Categories</NavDropdown.Item>
+                      <NavDropdown.Item href="/Dashboard">Dashboard</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="/signin"> LogOut</NavDropdown.Item>
+                    </NavDropdown>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
     
   );
   
--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import ScratchEditor from './scratcheditor';
-import Dashboard from './Dashboard';
+// import ScratchEditor from './scratcheditor';
+// import Dashboard from './Dashboard';
 
 
 
