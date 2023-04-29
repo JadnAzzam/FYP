e03efbfc6cc7d861e2a808e73247ae80b9499c98
JadnAzzam <jna26@mail.aub.edu>
2023-04-29 19:15:28 +0300

    update the language display logic

--- src/Navigation.js
+++ src/Navigation.js
@@ -9,10 +9,50 @@ import { useTranslation } from 'react-i18next';
 function Navigation() {
 
   const { t, i18n } = useTranslation();
+  const [selectedLanguages, setSelectedLanguages] = React.useState([]);
 
-  function handleClick(lang){
-    i18n.changeLanguage(lang);
+
+  // function handleClick(lang){
+  //   i18n.changeLanguage(lang);
+  // }
+
+  function handleCheckboxChange(e) {
+    const language = e.target.value;
+    const isChecked = e.target.checked;
+  
+    setSelectedLanguages(prevSelectedLanguages => {
+      if (isChecked) {
+        return [...prevSelectedLanguages, language];
+      } else {
+        return prevSelectedLanguages.filter(lang => lang !== language);
+      }
+    });
+  
+    // Updated logic for setting the displayed language
+    const updateDisplayedLanguage = (languages) => {
+      if (languages.length === 0) {
+        i18n.changeLanguage('en');
+      } else if (languages.length === 3 || languages.includes('en')) {
+        i18n.changeLanguage('en');
+      } else if (languages.includes('ar') && languages.includes('fr')) {
+        i18n.changeLanguage('ar');
+      } else if (languages.length === 1) {
+        i18n.changeLanguage(languages[0]);
+      } else {
+        i18n.changeLanguage('en'); // Default fallback
+      }
+    };
+  
+    // Call the updateDisplayedLanguage function with the new selectedLanguages state
+    updateDisplayedLanguage(
+      isChecked
+        ? [...selectedLanguages, language]
+        : selectedLanguages.filter(lang => lang !== language)
+    );
   }
+  
+  
+  
 
   return (
     <Navbar bg="light" expand="lg">
@@ -34,11 +74,24 @@ function Navigation() {
           </Nav>
         </Navbar.Collapse>
 
-        <div>
+        {/* <div>
         <button onClick={()=>handleClick('en')}>English</button>
         <button onClick={()=>handleClick('ar')}>Arabic</button>
         <button onClick={()=>handleClick('fr')}>French</button>
+        </div> */}
+
+        <div>
+          <label>
+            <input type="checkbox" value="en" onChange={handleCheckboxChange}/>English
+          </label>
+          <label>
+            <input type="checkbox" value="ar" onChange={handleCheckboxChange}/>Arabic
+          </label>
+          <label>
+            <input type="checkbox" value="fr" onChange={handleCheckboxChange}/>French
+          </label>
         </div>
+
 
         <p>{t('Hello')}</p>
         
